73f764fc4066ee25decfbdb9ed732748d7dcd0c2
Spencer-Verharst <spencev23@yahoo.com>
2025-12-20 14:21:20 -0600

    Update GolgBagDriver.cs

--- GolgBagDriver.cs
+++ GolgBagDriver.cs
@@ -23,26 +23,11 @@ namespace GolfBagManager
 5:   QUIT
 Whats your choice: 1
 
-Adding a new club to your bag...
-What type of club (Driver, Wood, Iron, Putter...): Driver
-Whats your club brand: TaylorMade Sim 2 Max
-How far does this club go (Yards): 275
-
-Driver added to bag.
-
-1:   Add Club
-2:   Remove Club
-3:   Get Number of Clubs
-4:   Display Bag
-5:   QUIT
-Whats your choice: 1
-
-Adding a new club to your bag...
-What type of club (Driver, Wood, Iron, Putter...): 3 Wood
-Whats your club brand: Ping G430 Max
-How far does this club go (Yards): 240
-
-3 Wood added to bag.
+Adding club...
+Enter Club Type (Driver, Wood, Iron, Wedge, Putter): Driver
+Enter Club Brand: Taylormade Sim 2 Max
+Enter Club Distance(Yards): 275
+Driver added to your bag!
 
 1:   Add Club
 2:   Remove Club
@@ -51,26 +36,12 @@ How far does this club go (Yards): 240
 5:   QUIT
 Whats your choice: 1
 
-Adding a new club to your bag...
-What type of club (Driver, Wood, Iron, Putter...): 5 Wood
-Whats your club brand: Ping G430 Max
-How far does this club go (Yards): 225
-
-5 Wood added to bag.
-
-1:   Add Club
-2:   Remove Club
-3:   Get Number of Clubs
-4:   Display Bag
-5:   QUIT
-Whats your choice: 1
-
-Adding a new club to your bag...
-What type of club (Driver, Wood, Iron, Putter...): 4 Iron
-Whats your club brand: Srixon ZXI 5
-How far does this club go (Yards): 200
-
-4 Iron added to bag.
+Adding club...
+Enter Club Type (Driver, Wood, Iron, Wedge, Putter): Wood
+Wood number (3-9): 3
+Enter Club Brand: Ping G430 Max
+Enter Club Distance(Yards): 240
+Wood added to your bag!
 
 1:   Add Club
 2:   Remove Club
@@ -79,26 +50,12 @@ How far does this club go (Yards): 200
 5:   QUIT
 Whats your choice: 1
 
-Adding a new club to your bag...
-What type of club (Driver, Wood, Iron, Putter...): 5 Iron
-Whats your club brand: Srixon ZXI 5
-How far does this club go (Yards): 190
-
-5 Iron added to bag.
-
-1:   Add Club
-2:   Remove Club
-3:   Get Number of Clubs
-4:   Display Bag
-5:   QUIT
-Whats your choice: 1
-
-Adding a new club to your bag...
-What type of club (Driver, Wood, Iron, Putter...): 6 Iron
-Whats your club brand: Srixon ZXI 5
-How far does this club go (Yards): 180
-
-6 Iron added to bag.
+Adding club...
+Enter Club Type (Driver, Wood, Iron, Wedge, Putter): Wood
+Wood number (3-9): 5
+Enter Club Brand: Ping G430 Max
+Enter Club Distance(Yards): 225
+Wood added to your bag!
 
 1:   Add Club
 2:   Remove Club
@@ -107,26 +64,12 @@ How far does this club go (Yards): 180
 5:   QUIT
 Whats your choice: 1
 
-Adding a new club to your bag...
-What type of club (Driver, Wood, Iron, Putter...): 7 Iron
-Whats your club brand: Srixon ZXI 7
-How far does this club go (Yards): 175
-
-7 Iron added to bag.
-
-1:   Add Club
-2:   Remove Club
-3:   Get Number of Clubs
-4:   Display Bag
-5:   QUIT
-Whats your choice: 1
-
-Adding a new club to your bag...
-What type of club (Driver, Wood, Iron, Putter...): 8 Iron
-Whats your club brand: Srixon ZXI 7
-How far does this club go (Yards): 160
-
-8 Iron added to bag.
+Adding club...
+Enter Club Type (Driver, Wood, Iron, Wedge, Putter): Iron
+Iron number (2-9): 4
+Enter Club Brand: Srixon ZXI 5
+Enter Club Distance(Yards): 200
+Iron added to your bag!
 
 1:   Add Club
 2:   Remove Club
@@ -135,26 +78,12 @@ How far does this club go (Yards): 160
 5:   QUIT
 Whats your choice: 1
 
-Adding a new club to your bag...
-What type of club (Driver, Wood, Iron, Putter...): 9 Iron
-Whats your club brand: Srixon ZXI 7
-How far does this club go (Yards): 150
-
-9 Iron added to bag.
-
-1:   Add Club
-2:   Remove Club
-3:   Get Number of Clubs
-4:   Display Bag
-5:   QUIT
-Whats your choice: 1
-
-Adding a new club to your bag...
-What type of club (Driver, Wood, Iron, Putter...): P Wedge
-Whats your club brand: Srixon ZXI 7
-How far does this club go (Yards): 130
-
-P Wedge added to bag.
+Adding club...
+Enter Club Type (Driver, Wood, Iron, Wedge, Putter): Iron
+Iron number (2-9): 5
+Enter Club Brand: Srixon ZXI 5
+Enter Club Distance(Yards): 190
+Iron added to your bag!
 
 1:   Add Club
 2:   Remove Club
@@ -163,26 +92,12 @@ P Wedge added to bag.
 5:   QUIT
 Whats your choice: 1
 
-Adding a new club to your bag...
-What type of club (Driver, Wood, Iron, Putter...): A Wedge
-Whats your club brand: Vokey SM9
-How far does this club go (Yards): 120
-
-A Wedge added to bag.
-
-1:   Add Club
-2:   Remove Club
-3:   Get Number of Clubs
-4:   Display Bag
-5:   QUIT
-Whats your choice: 1
-
-Adding a new club to your bag...
-What type of club (Driver, Wood, Iron, Putter...): S Wedge
-Whats your club brand: Vokey SM9
-How far does this club go (Yards): 110
-
-S Wedge added to bag.
+Adding club...
+Enter Club Type (Driver, Wood, Iron, Wedge, Putter): Iron
+Iron number (2-9): 6
+Enter Club Brand: Srixon ZXI 5
+Enter Club Distance(Yards): 180
+Iron added to your bag!
 
 1:   Add Club
 2:   Remove Club
@@ -191,12 +106,118 @@ S Wedge added to bag.
 5:   QUIT
 Whats your choice: 1
 
-Adding a new club to your bag...
-What type of club (Driver, Wood, Iron, Putter...): L Wedge
-Whats your club brand: Vokey SM9
-How far does this club go (Yards): 100
+Adding club...
+Enter Club Type (Driver, Wood, Iron, Wedge, Putter): Iron
+Iron number (2-9): 7
+Enter Club Brand: Srixon ZXI 7
+Enter Club Distance(Yards): 170
+Iron added to your bag!
 
-L Wedge added to bag.
+1:   Add Club
+2:   Remove Club
+3:   Get Number of Clubs
+4:   Display Bag
+5:   QUIT
+Whats your choice: 1
+
+Adding club...
+Enter Club Type (Driver, Wood, Iron, Wedge, Putter): Iron
+Iron number (2-9): 8
+Enter Club Brand: Srixon ZXI 7
+Enter Club Distance(Yards): 160
+Iron added to your bag!
+
+1:   Add Club
+2:   Remove Club
+3:   Get Number of Clubs
+4:   Display Bag
+5:   QUIT
+Whats your choice: 1
+
+Adding club...
+Enter Club Type (Driver, Wood, Iron, Wedge, Putter): Iron
+Iron number (2-9): 9
+Enter Club Brand: Srixon ZXI 7
+Enter Club Distance(Yards): 150
+Iron added to your bag!
+
+1:   Add Club
+2:   Remove Club
+3:   Get Number of Clubs
+4:   Display Bag
+5:   QUIT
+Whats your choice: 1
+
+Adding club...
+Enter Club Type (Driver, Wood, Iron, Wedge, Putter): Wedge
+Wedge type (PW, AW, SW, LW): PW
+Enter Club Brand: Srixon ZXI 7
+Enter Club Distance(Yards): 140
+Wedge added to your bag!
+
+1:   Add Club
+2:   Remove Club
+3:   Get Number of Clubs
+4:   Display Bag
+5:   QUIT
+Whats your choice: 1
+
+Adding club...
+Enter Club Type (Driver, Wood, Iron, Wedge, Putter): Wedge
+Wedge type (PW, AW, SW, LW): AW
+Enter Club Brand: Vokey SM9
+Enter Club Distance(Yards): 120
+Wedge added to your bag!
+
+1:   Add Club
+2:   Remove Club
+3:   Get Number of Clubs
+4:   Display Bag
+5:   QUIT
+Whats your choice: 1
+
+Adding club...
+Enter Club Type (Driver, Wood, Iron, Wedge, Putter): Wedge
+Wedge type (PW, AW, SW, LW): SW
+Enter Club Brand: Vokey SM9
+Enter Club Distance(Yards): 110
+Wedge added to your bag!
+
+1:   Add Club
+2:   Remove Club
+3:   Get Number of Clubs
+4:   Display Bag
+5:   QUIT
+Whats your choice: 1
+
+Adding club...
+Enter Club Type (Driver, Wood, Iron, Wedge, Putter): Wedge
+Wedge type (PW, AW, SW, LW): LW
+Enter Club Brand: Vokey SM9
+Enter Club Distance(Yards): 100
+Wedge added to your bag!
+
+1:   Add Club
+2:   Remove Club
+3:   Get Number of Clubs
+4:   Display Bag
+5:   QUIT
+Whats your choice: 1
+
+Adding club...
+Enter Club Type (Driver, Wood, Iron, Wedge, Putter): Putter
+Enter Club Brand: Scotty Cameron GoLo 5
+Enter Club Distance(Yards): 1
+Putter added to your bag!
+
+1:   Add Club
+2:   Remove Club
+3:   Get Number of Clubs
+4:   Display Bag
+5:   QUIT
+Whats your choice: 3
+
+You have 14/14 clubs in your bag
 
 1:   Add Club
 2:   Remove Club
@@ -205,22 +226,81 @@ L Wedge added to bag.
 5:   QUIT
 Whats your choice: 4
 
+
+========== Your Golf Bag ==========
+Clubs: 14/14
+
+1. Driver - Taylormade Sim 2 Max (275 yards)
+2. 3-Wood - Ping G430 Max (240 yards)
+3. 5-Wood - Ping G430 Max (225 yards)
+4. 4-Iron - Srixon ZXI 5 (200 yards)
+5. 5-Iron - Srixon ZXI 5 (190 yards)
+6. 6-Iron - Srixon ZXI 5 (180 yards)
+7. 7-Iron - Srixon ZXI 7 (170 yards)
+8. 8-Iron - Srixon ZXI 7 (160 yards)
+9. 9-Iron - Srixon ZXI 7 (150 yards)
+10. PW Wedge - Srixon ZXI 7 (140 yards)
+11. AW Wedge - Vokey SM9 (120 yards)
+12. SW Wedge - Vokey SM9 (110 yards)
+13. LW Wedge - Vokey SM9 (100 yards)
+14. Putter - Scotty Cameron GoLo 5 (1 yards)
+===================================
+
+1:   Add Club
+2:   Remove Club
+3:   Get Number of Clubs
+4:   Display Bag
+5:   QUIT
+Whats your choice: 2
+
+Clubs to remove...
+
+========== Your Golf Bag ==========
+Clubs: 14/14
+
+1. Driver - Taylormade Sim 2 Max (275 yards)
+2. 3-Wood - Ping G430 Max (240 yards)
+3. 5-Wood - Ping G430 Max (225 yards)
+4. 4-Iron - Srixon ZXI 5 (200 yards)
+5. 5-Iron - Srixon ZXI 5 (190 yards)
+6. 6-Iron - Srixon ZXI 5 (180 yards)
+7. 7-Iron - Srixon ZXI 7 (170 yards)
+8. 8-Iron - Srixon ZXI 7 (160 yards)
+9. 9-Iron - Srixon ZXI 7 (150 yards)
+10. PW Wedge - Srixon ZXI 7 (140 yards)
+11. AW Wedge - Vokey SM9 (120 yards)
+12. SW Wedge - Vokey SM9 (110 yards)
+13. LW Wedge - Vokey SM9 (100 yards)
+14. Putter - Scotty Cameron GoLo 5 (1 yards)
+===================================
+
+Enter a club type to remove: Putter
+Putter was removed from your bag.
+
+1:   Add Club
+2:   Remove Club
+3:   Get Number of Clubs
+4:   Display Bag
+5:   QUIT
+Whats your choice: 4
+
+
 ========== Your Golf Bag ==========
 Clubs: 13/14
 
-1. Driver - TaylorMade Sim 2 Max (275 yards)
-2. 3 Wood - Ping G430 Max (240 yards)
-3. 5 Wood - Ping G430 Max (225 yards)
-4. 4 Iron - Srixon ZXI 5 (200 yards)
-5. 5 Iron - Srixon ZXI 5 (190 yards)
-6. 6 Iron - Srixon ZXI 5 (180 yards)
-7. 7 Iron - Srixon ZXI 7 (175 yards)
-8. 8 Iron - Srixon ZXI 7 (160 yards)
-9. 9 Iron - Srixon ZXI 7 (150 yards)
-10. P Wedge - Srixon ZXI 7 (130 yards)
-11. A Wedge - Vokey SM9 (120 yards)
-12. S Wedge - Vokey SM9 (110 yards)
-13. L Wedge - Vokey SM9 (100 yards)
+1. Driver - Taylormade Sim 2 Max (275 yards)
+2. 3-Wood - Ping G430 Max (240 yards)
+3. 5-Wood - Ping G430 Max (225 yards)
+4. 4-Iron - Srixon ZXI 5 (200 yards)
+5. 5-Iron - Srixon ZXI 5 (190 yards)
+6. 6-Iron - Srixon ZXI 5 (180 yards)
+7. 7-Iron - Srixon ZXI 7 (170 yards)
+8. 8-Iron - Srixon ZXI 7 (160 yards)
+9. 9-Iron - Srixon ZXI 7 (150 yards)
+10. PW Wedge - Srixon ZXI 7 (140 yards)
+11. AW Wedge - Vokey SM9 (120 yards)
+12. SW Wedge - Vokey SM9 (110 yards)
+13. LW Wedge - Vokey SM9 (100 yards)
 ===================================
 
 1:   Add Club
@@ -230,9 +310,10 @@ Clubs: 13/14
 5:   QUIT
 Whats your choice: 5
 
-Thanks for using Golf Bag Manager!
+Thank you for using the GolfBag Manager!
 
  */
+
 
 
 
