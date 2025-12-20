85b42324af64deaa4dc466b19b4084c4bc8da03e
Spencer-Verharst <spencev23@yahoo.com>
2025-12-20 13:14:10 -0600

    Update Clubs.cs

--- Clubs.cs
+++ Clubs.cs
@@ -1,13 +1,13 @@
-﻿using System;
+using System;
 
 namespace GolfBagManager
 {
-    class Club
+    public abstract class Club
     {
-        public string Type { get; set; }
+        public string Type { get; }
         public string Brand { get; set; }
         public int Distance { get; set; }
-        public Club(string type, string brand, int distance)
+        protected Club(string type, string brand, int distance)
         {
             Type = type;
             Brand = brand;
@@ -20,5 +20,71 @@ namespace GolfBagManager
         }
     }
 
+    public class Driver : Club
+    {
+        public Driver(string brand, int distance)
+            : base("Driver", brand, distance) { }
+    }
 
+    public class Wood : Club
+    {
+        public int Number { get; set; }
+
+        public Wood(string brand, int distance, int number)
+            : base($"{number}-Wood", brand, distance)
+        {
+            Number = number;
+        }
+    }
+
+    public class Iron : Club
+    {
+        public int Number { get; set; }
+
+        public Iron(string brand, int distance, int number)
+            : base($"{number}-Iron", brand, distance)
+        {
+            Number = number;
+        }
+    }
+
+    public class Wedge : Club
+    {
+        public string WedgeType { get; set; }
+
+        public Wedge(string brand, int distance, string wedgeType)
+            : base($"{wedgeType} Wedge", brand, distance)
+        {
+            WedgeType = wedgeType;
+        }
+    }
+
+    public class Putter : Club
+    {
+        public Putter(string brand, int distance)
+            : base("Putter", brand, distance) { }
+    }
+
+    public class GenericClub : Club
+    {
+        public GenericClub(string type, string brand, int distance)
+            : base(type, brand, distance) { }
+    }
+
+    public class ClubFactory
+    {
+        public Club? CreateClub(string type, string brand, int distance, int? number = null, string? wedgeType = null)
+        {
+            return type.ToLower() switch
+            {
+                "driver" => new Driver(brand, distance),
+                "putter" => new Putter(brand, distance),
+                "wood" => new Wood(brand, distance, number ?? 3),
+                "iron" => new Iron(brand, distance, number ?? 5),
+                "wedge" => new Wedge(brand, distance, wedgeType ?? "PW"),
+                _ => new GenericClub(type, brand, distance)
+            };
+        }
+    }
 }
+
